2f5bc2a0c6a8981a7ac0c18b331a68a486ed52bd
lempiy <lempiyada@gmail.com>
2018-06-02 12:32:16 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pok-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'pok';
+}
